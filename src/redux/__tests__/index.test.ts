752be063f03f07fbefebfba5a46fbcea3295950e
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2021-09-20 07:33:51 -0300

    spyon configured

--- src/redux/__tests__/index.test.ts
+++ src/redux/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import * as redux from 'redux';
 import * as reduxPersist from 'redux-persist';
 import * as reduxSaga from 'redux-saga';
 
-import {configureStore} from '@redux';
+import {configureStore} from '../store';
 
 jest.mock('@react-native-community/async-storage', () => ({}));
 
@@ -14,4 +14,47 @@ describe('Fluxo da store', () => {
     key: 'root',
     storage: AsyncStorage,
   };
+
+  const persistReducer = jest.spyOn(reduxPersist, 'persistReducer');
+
+  const createSagaMiddleware = jest.spyOn(reduxSaga, 'default');
+
+  const createStore = jest.spyOn(redux, 'createStore');
+  const applyMiddleware = jest.spyOn(redux, 'applyMiddleware');
+
+  const mockPersistStoreReturn = {
+    dispatch: jest.fn(),
+    pause: jest.fn(),
+    persist: jest.fn(),
+    purge: jest.fn(),
+    flush: jest.fn(),
+    getState: jest.fn(),
+    subscribe: jest.fn(),
+  };
+
+  const persistStore = jest
+    .spyOn(reduxPersist, 'persistStore')
+    .mockReturnValue(mockPersistStoreReturn);
+
+  test('deve configurar a saga corretamente instanciando as sagas e retornando o persistor e a store', () => {
+    const {persistor, store} = configureStore();
+    const sagaMiddleware = createSagaMiddleware.mock.results[0].value;
+
+    expect(persistReducer).toHaveBeenCalledWith(persistConfig, rootReducer);
+    expect(createSagaMiddleware).toHaveBeenCalled();
+    expect(applyMiddleware).toHaveBeenCalledWith(sagaMiddleware);
+
+    expect(createStore).toHaveBeenCalledWith(
+      persistReducer.mock.results[0].value,
+      {},
+      expect.any(Function),
+    );
+
+    expect(persistStore).toHaveBeenCalledWith(
+      createStore.mock.results[0].value,
+    );
+
+    expect(persistor).toEqual(persistStore.mock.results[0].value);
+    expect(store).toEqual(createStore.mock.results[0].value);
+  });
 });
